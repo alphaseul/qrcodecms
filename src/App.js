756c0702bf13d06306e336dd46b073865ed3d736
alphaseul <alphaseul@yahoo.fr>
2020-10-19 22:51:53 +0200

    creation path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import  {
+    BrowserRouter as Router,
+    Route,
+    Switch
+
+} from 'react-router-dom';
+import Home from "./Home";
+import Login from "./Login"
+import NoPath from "./404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div>
+              <Header/>
+              <Switch>
+                  <Route exact path="/">
+                      <Home/>
+                  </Route>
+                  <Route path="/login">
+                      <Login/>
+                  </Route>
+                  <Route path="*">
+                      <NoPath/>
+                  </Route>
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
