59f2b417741831a7510eec37000859fff65da4f0
alphaseul <alphaseul@yahoo.fr>
2020-11-16 20:25:58 +0100

    Updates

---
+++ precache-manifest.a246006735fcbc2793d28ee42efaffae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e61aeee34a4d9eef9a9d2ceda26012a",
+    "url": "/alphaseul/qrcodecms/index.html"
+  },
+  {
+    "revision": "a657faa41d5267516c0e",
+    "url": "/alphaseul/qrcodecms/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "4177a32a0f2905f1a719",
+    "url": "/alphaseul/qrcodecms/static/css/main.f3b8c075.chunk.css"
+  },
+  {
+    "revision": "a657faa41d5267516c0e",
+    "url": "/alphaseul/qrcodecms/static/js/2.9c406033.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/alphaseul/qrcodecms/static/js/2.9c406033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4177a32a0f2905f1a719",
+    "url": "/alphaseul/qrcodecms/static/js/main.06b9c2b7.chunk.js"
+  },
+  {
+    "revision": "74e23bd0a4d0a1d04266",
+    "url": "/alphaseul/qrcodecms/static/js/runtime-main.673fe96a.js"
+  },
+  {
+    "revision": "b736dd4fbf42a95d0a4b80bf3fcc97fd",
+    "url": "/alphaseul/qrcodecms/static/media/qrcode-logo.b736dd4f.png"
+  }
+]);
--- src/services/auth-header.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    const jwt = JSON.parse(localStorage.getItem('jwt'));
-  
-    if (jwt) {
-      return { Authorization: 'Bearer ' + jwt };
-    } else {
-      return {};
-    }
-  }
