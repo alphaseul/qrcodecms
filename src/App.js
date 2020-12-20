be08e7d06b245f20ae2a1443390e5557e9481bd9
alphaseul <alphaseul@yahoo.fr>
2020-12-20 21:40:46 +0100

    format code

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Router } from "react-router-dom";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Router} from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./Footer";
-import { logout } from "./actions/auth";
-import { clearMessage } from "./actions/message";
-import { history } from "./helpers/history";
+import {logout} from "./actions/auth";
+import {clearMessage} from "./actions/message";
+import {history} from "./helpers/history";
 import SideBar from "./SideBar";
 import MyRoute from "./Route";
 import NavBar from "./NavBar";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.logOut = this.logOut.bind(this);
-    this.state = {
-      currentUser: undefined,
-    };
-    history.listen((location) => {
-      props.dispatch(clearMessage());
-    });
-  }
-
-  componentDidMount() {
-    const user = this.props.user;
-    document.title = "QR-code protect";
-
-    if (user) {
-      this.setState({
-        currentUser: user,
-      });
+    constructor(props) {
+        super(props);
+        this.logOut = this.logOut.bind(this);
+        this.state = {
+            currentUser: undefined,
+        };
+        history.listen((location) => {
+            props.dispatch(clearMessage());
+        });
     }
-  }
 
-  logOut() {
-    this.props.dispatch(logout());
-  }
+    componentDidMount() {
+        const user = this.props.user;
+        document.title = "QR-code protect";
 
-  render() {
-    const { currentUser } = this.state;
+        if (user) {
+            this.setState({
+                currentUser: user,
+            });
+        }
+    }
 
-    return (
-      <div>
-        <Router history={history}>
-          <NavBar currentUser={!currentUser} />
-          <SideBar logOut={this.logOut} currentUser={currentUser} />
-          <MyRoute currentUser={currentUser} />
-          <Footer currentUser={!currentUser} />
-        </Router>
-      </div>
-    );
-  }
+    logOut() {
+        this.props.dispatch(logout());
+    }
+
+    render() {
+        const {currentUser} = this.state;
+
+        return (
+            <div>
+                <Router history={history}>
+                    <NavBar currentUser={!currentUser}/>
+                    <SideBar logOut={this.logOut} currentUser={currentUser}/>
+                    <MyRoute currentUser={currentUser}/>
+                    <Footer currentUser={!currentUser}/>
+                </Router>
+            </div>
+        );
+    }
 }
 
 function mapStateToProps(state) {
-  const { user } = state.auth;
-  return {
-    user,
-  };
+    const {user} = state.auth;
+    return {
+        user,
+    };
 }
 
 export default connect(mapStateToProps)(App);
